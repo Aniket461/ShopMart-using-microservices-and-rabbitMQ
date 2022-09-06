85073677010e8fb5a18d7a994801c7bd2a2c8183
Aniket461 <surveaniket461@gmail.com>
2022-09-06 19:27:25 +0530

    Fixing the AUth file issue

--- shared-services/Auth.js
+++ shared-services/Auth.js
@@ -4,14 +4,16 @@ require('dotenv').config({path:'./.env'});
 
 module.exports = async function isAuthenticated(req,res,next){
 
-    if(!req.headers["authorization"]){
+    if(req.headers["authorization"] === undefined){
         res.status(500).json({"message":"Something went wrong with login, please login again from headers"});
+        return;
     }
 
     const token = req.headers["authorization"].split(" ")[1];
     jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
         if(err){
             res.status(500).json({"message":"Something went wrong with login, please login again from verified"});
+            return;
         }
         else{
             console.log(user);
